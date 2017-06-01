ce43485ecd5b093dd96a8d14cbed0e15a9e77c71
Ranc58 <rancvova@gmail.com>
2017-06-01 10:39:36 +0300

    add create_filepath_to_save_image func

--- image_resize.py
+++ image_resize.py
@@ -7,7 +7,7 @@ from PIL import Image
 def create_parser_for_user_arguments():
     parser = argparse.ArgumentParser(description='Resize image.')
     parser.add_argument('infile', help='Way to image for resize', type=str)
-    parser.add_argument('-s', '--scale', nargs='?', help='Resize image by scale', type=float, const=only_scale_resize)
+    parser.add_argument('-s', '--scale', nargs='?', help='Resize image by scale', type=float)
     parser.add_argument('-ws', '--width', nargs='?', help='Width of out image', type=int)
     parser.add_argument('-hs', '--height', nargs='?', help='Height of out image', type=int)
     parser.add_argument('-o', '--outfile', nargs='?', help='Way and name of image to save', type=str)
@@ -22,8 +22,8 @@ def only_width(width, infile, outfile):
 
 def only_height(height, infile, outfile):
     im = Image.open(infile)
-    new_width = int(im.size[0] / (im.size[1] / height))
-    im.resize((new_width, height)).save(outfile)
+    new_width = int(im.size[1]/(im.size[0] / height))
+    im.resize((height, new_width)).save(outfile)
 
 
 def width_and_height(width, height, infile, outfile):
@@ -38,22 +38,29 @@ def only_scale_resize(scale, infile, outfile):
     im.resize((new_height, new_width)).save(outfile)
 
 
-def create_filepath_to_save_image():
-    pass
+def create_filepath_to_save_image(infile, width, height):
+    root, ext = os.path.splitext(infile)
+    print(root,ext)
 
 
 if __name__ == '__main__':
+    infile='text.jpg'
+    width=300
+    height=300
+    create_filepath_to_save_image(infile, width, height)
+"""
     parser = create_parser_for_user_arguments()
-    user_argument = parser.parse_args(sys.argv[1:])
-    print(user_argument)
+    user_argument = parser.parse_args(sys.argv[1:])    
     if user_argument.scale:
         only_scale_resize(user_argument.scale, user_argument.infile, user_argument.outfile)
     elif user_argument.width and user_argument.height:
         width_and_height(user_argument.width, user_argument.height, user_argument.infile, user_argument.outfile)
     elif user_argument.width:
+        print('wid')
         only_width(user_argument.width, user_argument.infile, user_argument.outfile)
     elif user_argument.height:
+        print('height')
         only_height(user_argument.height, user_argument.infile, user_argument.outfile)
     else:
         print('Please enter correct arguments!')
-
+"""
